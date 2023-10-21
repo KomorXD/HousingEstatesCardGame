b12cc04c93d4223696e823725fc258bee252867e
Mikolaj <mikus22500@gmail.com>
2023-10-21 20:12:55 +0200

    Display card on board tiles

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    private GameObject _card;
+    private Vector3 _cardPosition;
+
+    public GameObject cardToDisplay;
+
+    private void Start()
+    {
+        _cardPosition = transform.position;
+        _cardPosition.y += 0.01f;
+    }
+
+    private void OnMouseDown()
+    {
+        DisplayCard(cardToDisplay);
+    }
+
+    public void DisplayCard(GameObject card)
+    {
+        GameObject _env = GameObject.FindGameObjectWithTag("BoardTag");
+        _card = card;
+        _card.transform.position = _cardPosition;
+        _card = Instantiate(card, _env.transform);
+    }
+}
