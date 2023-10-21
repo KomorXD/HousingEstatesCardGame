f3aacec82dda41235a8d23c566061a1dce05e73e
Mikolaj <mikus22500@gmail.com>
2023-10-21 19:44:06 +0200

    Add simple board creation

--- Assets/Scripts/BoardScript.cs
+++ Assets/Scripts/BoardScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BoardScript : MonoBehaviour
@@ -11,19 +12,32 @@ public class BoardScript : MonoBehaviour
 
     void Start()
     {
-        GameObject sqr = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        sqr.transform.position = new Vector3(0.5f, 0.0f, 0.5f);
-        sqr.transform.localScale = Vector3.one;
-        sqr.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
-
-        _grid = new List<GameObject>
-        {
-            sqr
-        };
+        PopulateBoard();
     }
     
     void Update()
     {
         
+    }
+
+    void PopulateBoard()
+    {
+        GameObject _env = GameObject.FindGameObjectWithTag("BoardTag");
+        GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        _grid = new List<GameObject>();
+        int offset = 2;
+
+        tile.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                GameObject sqr = Instantiate(tile, _env.transform);
+                sqr.transform.position = new Vector3(offset * i, 0.0f, offset * j);
+                sqr.transform.localScale = offset * Vector3.one;
+                _grid.Add(sqr);
+            }
+        }
+        Destroy( tile );
     }
 }
