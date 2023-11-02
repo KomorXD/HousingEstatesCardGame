546e36da0575bd25ace938d0701a1f72de29946c
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-11-02 18:52:55 +0100

    Serialize things, add bombs count

--- Assets/Scripts/Managers/DifficultiesManager.cs
+++ Assets/Scripts/Managers/DifficultiesManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public struct DifficultyRequirement
 {
     public CardParameter Parameter;
@@ -26,16 +27,19 @@ public struct DifficultyRequirement
     }
 }
 
+[System.Serializable]
 public struct Difficulty
 {
     public string Name;
     public string IconPath;
+    public int BombsCount;
     public List<DifficultyRequirement> Requirements;
 
-    public Difficulty(string name, string iconPath, List<DifficultyRequirement> reqs)
+    public Difficulty(string name, string iconPath, int bombsCount, List<DifficultyRequirement> reqs)
     {
         Name = name;
         IconPath = iconPath;
+        BombsCount = bombsCount;
         Requirements = reqs;
     }
 }
@@ -44,27 +48,27 @@ public class DifficultiesManager : MonoBehaviour
 {
     public static DifficultiesManager Instance { get; private set; }
 
-    private List<Difficulty> difficulties;
+    [SerializeField] private List<Difficulty> difficulties;
     public List<Difficulty> Difficulties => difficulties;
 
     public void Init()
     {
         difficulties = new();
         
-        Difficulty diff = new Difficulty("Easy", "Textures/Difficulties/easy_icon", new()
+        Difficulty diff = new Difficulty("Easy", "Textures/Difficulties/easy_icon", 10, new()
         {
             new DifficultyRequirement(new CardParameter(ParameterCategory.Trees, 10), 1, 100)
         });
         difficulties.Add(diff);
 
-        diff = new Difficulty("Normal", "Textures/Difficulties/normal_icon", new()
+        diff = new Difficulty("Normal", "Textures/Difficulties/normal_icon", 5, new()
         {
             new DifficultyRequirement(new CardParameter(ParameterCategory.GreenSpaceIndex, 11), 2, 200),
             new DifficultyRequirement(new CardParameter(ParameterCategory.DwellingsPerHa, 12), 3, 300)
         });
         difficulties.Add(diff);
 
-        diff = new Difficulty("Hard", "Textures/Difficulties/hard_icon", new()
+        diff = new Difficulty("Hard", "Textures/Difficulties/hard_icon", 2, new()
         {
             new DifficultyRequirement(new CardParameter(ParameterCategory.FloorRatio, 13), 3, 300),
             new DifficultyRequirement(new CardParameter(ParameterCategory.AverageFloors, 14), 4, 400)
@@ -87,7 +91,7 @@ public class DifficultiesManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-        DontDestroyOnLoad(gameObject);
         Init();
+        DontDestroyOnLoad(gameObject);
     }
 }
